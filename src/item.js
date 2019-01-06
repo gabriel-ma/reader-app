e30fe0e9731ace274b469ac97175772b5b73a8b2
Gabriel Machado <GABRIEL.MACHADO-BANDEIRA@SONICDRIVEIN.COM>
2019-01-06 15:05:20 -0200

    the test for the hostgator hosting

---
+++ src/item.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Item extends Component {
+    render() {
+        return (
+        <div>
+            The first manwha
+        </div>)
+    }
+}
+
+export default Item;
