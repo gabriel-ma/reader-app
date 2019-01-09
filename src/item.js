f24db4e6c14a637261891d9c977171568d0e6c0f
Gabriel Machado <GABRIEL.MACHADO-BANDEIRA@SONICDRIVEIN.COM>
2019-01-09 15:17:06 -0300

    first test for displaying content

--- src/item.js
+++ src/item.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
 class Item extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            title: []
+        }
+    }
+    componentDidMount() {
+        fetch("http://reader-rookiez-scans.com.br/api.php?title_name=Capture&chapter=2")
+        .then(results => {
+            return results.json();
+        }).then(data => {
+            console.log(data);
+            let title_data = 
+                
+                    <div>
+                        <p>{data.title_name}</p>
+                        {data.pages.map( page => {
+                            let image_source = 'http://www.reader-rookiez-scans.com.br/reader/Capture/2/' + page;
+                            return (
+                            <div styles={{left:'20%'}}>
+                                          <img src={image_source} styles={{width:'720px'}}/>	
+                		    </div>)
+                        }
+
+                        )}
+                    </div>
+                
+            
+            this.setState({title:title_data})
+        }
+        )
+    }
     render() {
         return (
         <div>
-            The first manwha
+            {this.state.title}
         </div>)
     }
 }
