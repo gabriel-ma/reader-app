56dfb5f9367aaf81ce905609344cd0ea97f46c91
Gabriel Machado <GABRIEL.MACHADO-BANDEIRA@SONICDRIVEIN.COM>
2019-01-09 23:34:57 -0300

    start the responsive exibition of images

--- src/item.js
+++ src/item.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
-class Item extends Component {
 
+
+class Item extends Component {
+    
     constructor() {
         super();
         this.state = {
@@ -16,19 +18,23 @@ class Item extends Component {
         }).then(data => {
             console.log(data);
             let title_data = 
+            <div className={'row'}>
                 
-                    <div>
+                <div className={'col-sm-2'} style={{display:'block'}}></div>
+                    <div className={'col-sm-8'}>
                         <p>{data.title_name}</p>
                         {data.pages.map( page => {
                             let image_source = 'http://www.reader-rookiez-scans.com.br/reader/Capture/4/' + page + '.png';
                             return (
-                            <div style={{left:'20%'}} className={'item'}>
-                                          <img src={image_source} className={'img-fluid'} style={{width:'720px'}}/>	
+                            <div className={'item'}>
+                                          <img src={image_source} className={'img-fluid'} style={{width:'720px', display: 'block',marginLeft: 'auto',marginRight: 'auto'}}/>	
                 		    </div>)
                         }
 
                         )}
                     </div>
+                    <div className={'col-sm-2'}></div>
+                 </div>
                 
             
             this.setState({title:title_data})
@@ -36,8 +42,13 @@ class Item extends Component {
         )
     }
     render() {
+        let bgColors = { "Default": "#81b71a",
+    "Blue": "#00B1E1",
+    "Cyan": "#37BC9B",
+    "Green": "#8CC152","Red": "#E9573F","Yellow": "#F6BB42",
+};
         return (
-        <div>
+        <div className={'container-fluid'} style={{backgroundColor:bgColors.Yellow}}>
             {this.state.title}
         </div>)
     }
